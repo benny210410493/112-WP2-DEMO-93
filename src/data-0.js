12a7657e5830d33e7ba0e6faa1fcc19fc0a475d5
benny210410493 <bennyjou1003@gmail.com>
2023-02-15 20:04:51 +0800

    first commit

---
+++ src/data-0.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    id: 1,
+    title: 'Samsung Galaxy S7',
+    price: 599.99,
+    img:
+      'https://vydnasfantyodcyrgsjs.supabase.co/storage/v1/object/public/demo-93/Samsung%20Galaxy%20S7.png',
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: 'google pixel ',
+    price: 499.99,
+    img:
+      'https://vydnasfantyodcyrgsjs.supabase.co/storage/v1/object/public/demo-93/Google%20Pixel.png',
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: 'Xiaomi Redmi Note 2',
+    price: 699.99,
+    img:
+      'https://vydnasfantyodcyrgsjs.supabase.co/storage/v1/object/public/demo-93/Xiaomi%20Redmi%20Note%202.png',
+    amount: 1,
+  },
+  {
+    id: 4,
+    title: 'Samsung Galaxy S8',
+    price: 699.99,
+    img:
+      'https://vydnasfantyodcyrgsjs.supabase.co/storage/v1/object/public/demo-93/Samsung%20Galaxy%20S8.png',
+    amount: 1,
+  },
+];
